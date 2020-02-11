cc46a481de4fdef50ab34742b5c4420913c6579e
hkarrson <hkarrson@gmail.com>
2020-02-11 10:51:38 -0800

    Progress

--- Gem/AST.py
+++ Gem/AST.py
@@ -8,6 +8,24 @@ class Node(object):
                 self.__dict__ == other.__dict__)
     def __ne__(self, other):
         return not (self == other)
+class Block(Node):
+    def __init__(self, statements):
+        self.statements = statements
+class Statement(Node):
+    def __init__(self, expr):
+        self.expr = expr
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+class Name(Node):
+    def __init__(self, name):
+        self.name = name
+class String(Node):
+    def __init__(self, text):
+        self.text = text
+class Return(Node):
+    def __init__(self, expr):
+        self.expr = expr
 
 pg = ParserGenerator([
                        "STRING",
@@ -31,8 +49,27 @@ pg = ParserGenerator([
 def main(s):
     return s[0]
 
+@pg.production("statements : LBRACE statements statement RBRACE")
+def statements(s):
+    return Block(s[1].getastlist() + [s[2]])
+
+@pg.production("statements : LBRACE statement RBRACE")
+def statements_statement(s):
+    return Block([s[1]])
+
+@pg.production("statement : expr SEMICOLON")
+def statement_expr(s):
+    return Statement(s[0])
+
+@pg.production("expr : NUMBER")
+def expr_number(s):
+    return Number(float(s[0].getstr()))
+
+@pg.production("expr : STRING")
+def expr_string(s):
+    return String(s[0].getstr()[1:-1])
+
 parser = pg.build()
 
 def BuildAST(LexedSource):
-    
-    return None
+    return parser.parse(LexedSource)
--- Gem/Gem.py
+++ Gem/Gem.py
@@ -7,7 +7,7 @@ def main(argv):
     for Token in LexedSource:
         print(Token)
     ASTInstance = AST.BuildAST(LexedSource)
-    
+    print(ASTInstance)
     
 if __name__== "__main__":
     main(sys.argv[1:])
