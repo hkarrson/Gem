dbcad34c9796110775679ed19542f13dbd222618
hkarrson <hkarrson@gmail.com>
2020-02-20 20:02:31 -0800

    Lexing Utility & Lexer Start

---
+++ Gem/LexingUtil.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Gem
+{
+    public static class LexingUtil
+    {
+        private static string Src = "";
+        private static Dictionary<int, string> Matches1 = new Dictionary<int, string>();
+        private static Dictionary<int, string> Matches2 = new Dictionary<int, string>();
+        private static List<string> IgnorePatterns = new List<string>();
+
+        public static void SetSource(string Src)
+        {
+            IgnorePatterns.Clear();
+            Matches1.Clear();
+            Matches2.Clear();
+            LexingUtil.Src = Src;
+        }
+
+        public static void Ignore(string Pattern)
+        {
+            IgnorePatterns.Add(Pattern);
+        }
+
+        public static void Add(string Name, string Pattern)
+        {
+            List<string> Src1 = new List<string>();
+            Src1.Add(Src);
+            foreach (string p in IgnorePatterns)
+            {
+                string[] Src3 = Src1.ToArray();
+                Src1.Clear();
+                foreach (string Src2 in Src3)
+                {
+                    Src1.AddRange(Regex.Split(Src2, p).ToList());
+                }
+            }
+            foreach (string Src4 in Src1)
+            {
+                foreach (Match m in Regex.Matches(Src4, Pattern))
+                {
+                    Matches1.Add(m.Index, m.Value);
+                    Matches2.Add(m.Index, Name);
+                }
+            }
+            List<KeyValuePair<int, string>> Lst = Matches1.ToList().OrderBy(KVP => KVP.Key).ToList();
+            Matches1.Clear();
+            foreach (KeyValuePair<int, string> KVP in Lst)
+            {
+                Matches1.Add(KVP.Key, KVP.Value);
+            }
+            Lst = Matches2.ToList().OrderBy(KVP => KVP.Key).ToList();
+            Matches2.Clear();
+            foreach (KeyValuePair<int, string> KVP in Lst)
+            {
+                Matches2.Add(KVP.Key, KVP.Value);
+            }
+        }
+
+        public class Token
+        {
+            public string Name, Value;
+        }
+
+        public static List<Token> Pop()
+        {
+            List<Token> Lst = new List<Token>();
+            int i = 0;
+            foreach (string value in Matches1.Values.ToList())
+            {
+                string name = Matches2.Values.ToList()[i];
+                Lst.Add(new Token() { Name = name, Value = value });
+                i++;
+            }
+            Src = "";
+            Matches1.Clear();
+            Matches2.Clear();
+            IgnorePatterns.Clear();
+            return Lst;
+        }
+    }
+}
--- Gem/Program.cs
+++ Gem/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Gem
 {
@@ -8,9 +10,20 @@ namespace Gem
         static void Main(string[] args)
         {
             string[] Src = File.ReadAllLines(args[0]);
+            List<LexingUtil.Token> Tokens = new List<LexingUtil.Token>();
             foreach (string Line in Src)
             {
-                Console.WriteLine(Line);
+                LexingUtil.SetSource(Line);
+                LexingUtil.Add("STRING", @"\"".*\""");
+                LexingUtil.Ignore(@"\s+");
+                LexingUtil.Add("GLOBAL", @"global");
+                LexingUtil.Add("HIDDEN", @"hidden");
+                LexingUtil.Add("END", @"end");
+                Tokens.AddRange(LexingUtil.Pop());
+            }
+            foreach (LexingUtil.Token Token in Tokens)
+            {
+                Console.WriteLine(Token.Name + " ::: " + Token.Value);
             }
             Console.ReadKey();
         }
